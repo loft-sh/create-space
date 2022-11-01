d216d54c48b2804341289c7db195feb21267c42c
Russell Centanni <russell.centanni@gmail.com>
2022-11-01 16:14:02 -0400

    build: update dependencies and fix build errors

--- src/post.ts
+++ src/post.ts
@@ -19,7 +19,9 @@ async function run(): Promise<void> {
       await exec('loft', args.build())
     }
   } catch (error) {
-    core.setFailed(error.message)
+    if (error instanceof Error) {
+      core.setFailed(error.message)
+    }
   }
 }
 
